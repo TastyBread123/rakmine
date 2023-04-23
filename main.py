737a8f0eac6108859b3a53aae1014206ee1296ac
TastyBread123 <117034601+TastyBread123@users.noreply.github.com>
2023-04-23 17:14:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from functools import partial
 mineflayer = require('mineflayer')
 pathfinder = require('mineflayer-pathfinder')
 maps = require('mineflayer-maps')
-pathfinder = require('mineflayer-pathfinder')
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -52,40 +51,19 @@ class RakMineSettings:
         window.mainloop()
 
 
-# Главное меню
-class RakMineMain:
+class RakMineJoin:
     def __init__(self, window: CTk) -> None:
-        self.in_game = False
+        nick_input = CTkEntry(master=window, width=213, placeholder_text='Ник бота')
+        nick_input.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
+        nick_input.insert(INSERT, config.get('Main', 'username'))
+        host_input = CTkEntry(master=window, width=250, placeholder_text='Адрес сервера формата ip:port / host')
+        host_input.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
+        host_input.insert(INSERT, config.get('Main', 'host'))
+        version_input = CTkEntry(master=window, width=70, placeholder_text='Версия')
+        version_input.place(relx=0.5, rely=0.4, anchor=tkinter.CENTER)
+        version_input.insert(INSERT, config.get('Main', 'version'))
 
-        def send_message():
-            global bot
-            if bot is None:
-                cmd_input.delete(0, END)
-                return showerror('Ошибка RakMine', 'Бот не запущен на сервере!')
-            
-            if cmd_input.get().startswith(prefix):
-                cmd = cmd_input.get().replace('$', '')
-                #if cmd not in cmds: return log(f'| [CMD] Команда "{cmd}" не найдена!')
-                if cmd == 'exit':
-                    bot.quit(); bot = None; self.in_game = False
-                    self.log('[CMD] Бот успешно вышел из игры!')
-                    return cmd_input.delete(0, END)
-                
-                if cmd.startswith('move'):
-                    pos = str(cmd.replace('move ', '')).split(' ')
-                    if len(pos) != 3:
-                        return self.log('[CMD] Использование: $move x y z')
-                    bot.pathfinder.setMovements(pathfinder.Movements(bot))
-                    bot.pathfinder.setGoal(pathfinder.goals.GoalNear(int(pos[0]), int(pos[1]), int(pos[2]), 1))
-                
-                return True
-            
-            bot.chat(cmd_input.get())
-            return cmd_input.delete(0, END)
-
-        def bot_join():
-            global bot
-            
+        def bot_join():            
             if nick_input.get() is None or nick_input.get() == '':
                 return showerror('Ошибка RakMine', 'Вы не ввели имя пользователя!')
             elif host_input.get() is None or host_input.get() == '':
@@ -93,102 +71,24 @@ class RakMineMain:
             elif version_input.get() is None or version_input.get() == '':
                 return showerror('Ошибка RakMine', 'Вы не ввели версию игры!')
             
-            if bot is not None: bot.quit()
+            window = CTk()
+            window.title("RakMine")
+            window.geometry('1000x620')
+            RakMineMain(window, host_input.get(), nick_input.get(), version_input.get())
             
-            ip = host_input.get().split(':')
-            try: port = ip[1]
-            except IndexError: port = 25565
+        join_button = CTkButton(master=window, text='Запустить', command=bot_join)
+        join_button.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
-            # Настройки
-            bot = mineflayer.createBot({#для бота
-                'host': ip[0],
-                'version': version_input.get(),
-                'port': port,
-                'maps_outputDir': r"maps",
-                'maps_saveToFile': True,
-                'username': nick_input.get()})
-
-            bot.loadPlugin(maps.inject)
-            bot.loadPlugin(pathfinder.pathfinder)
-
-            config.set('Main', 'username', nick_input.get())
-            config.set('Main', 'version', version_input.get())
-            config.set('Main', 'host', host_input.get())
-            with open('config.ini', "w") as config_file: config.write(config_file)
-            
-            if int(config.get('Settings', 'clear_chatlog')):
-                try: self.main_label.configure(text = check_stat())
-                except: pass
-                self.chat_log.delete(0.0, END)
-                window.title(f"{nick_input.get()} | {host_input.get()}")
+        window.mainloop()
 
 
-            @On(bot, 'error')
-            def error_handler(emittter, err):
-                return self.log(f'| [BOT ERROR] {err}')
-
-            @On(bot, 'message')
-            def message_handler(emitter, message_info, *args):
-                if message_info.extra is None: return False
-
-                new_text = ''
-                for i in message_info.extra: new_text+=str(i.text)
-                return self.log(f'[MSG] {new_text}')
-
-            @On(bot, 'chat')
-            def message_handler(emitter, username, message, *args):
-                if message is None: return False
-                return self.log(f'[CHAT] {username}: {message}')
-
-            @On(bot, 'spawn')
-            def spawn_handle(*args):
-                self.in_game = True
-                try: return self.log(f"| [BOT INFO] Бот заспавнен на {bot.entity.position.toString()}!")
-                except: pass
-            
-            @On(bot, 'death')
-            def spawn_handle(*args):
-                self.in_game = True
-                try: return self.log(f"| [BOT INFO] Бот помер на {bot.entity.position.toString()}!")
-                except: pass
-            
-            @On(bot, "end")
-            def end_handle(emitter, reason):
-                global bot
-                self.in_game = False; bot = None
-                self.log(f"| [BOT INFO] Бот отключен от сервера!\n| Причина: {reason}")
-                return self.main_label.configure(text = check_stat())
-            
-            @On(bot, 'move')
-            def move_handle(*args):
-                return self.main_label.configure(text = check_stat())
-            
-            @On(bot, "kicked")
-            def end_handle(emitter, reason, loddedin):
-                global bot
-                self.in_game = False; bot = None
-                
-                self.log(f"| [BOT INFO] Бот кикнут из сервера!\n| Причина: {reason}")
-                return self.main_label.configure(text = check_stat())
-            
-            @On(bot, "health")
-            def end_handle(*args):
-                self.in_game = True
-                return self.main_label.configure(text = check_stat())
-            
-            @On(bot, "forcedMove")
-            def forcedMove(*args):
-                self.in_game = True
-                try:
-                    p = bot.entity.position
-                    return self.log(f"| [BOT INFO] Бот телепортирован на {p.toString()}")
-                except: pass
-            
-            @On(bot, 'windowOpen')
-            def window_handler(emitter, window):
-                return print(window)
-
-
+# Главное меню
+class RakMineMain:
+    def __init__(self, window: CTk, host: str, nick: str, version: str) -> None:
+        def send_message():
+            cmd_input.delete(0.0, END)
+            return bot.chat(cmd_input.get())
+        
         #bot.setControlState(control, state) | 'forward', 'back', 'left', 'right', # 'jump', 'sprint', 'sneak'
         def control_click(control: str, button: CTkButton):
             if bot is None: return showerror('Ошибка RakMine', 'Бот не запущен на сервере!')
@@ -198,28 +98,18 @@ class RakMineMain:
             else:
                 bot.setControlState(control, False)
                 return button.configure(fg_color = '#4CAF50')
+        
+        global bot
 
-        #Основа
         self.main_label = CTkLabel(master = window, text=str(check_stat()))
         self.main_label.place(relx=0.5, rely = 0.03, anchor=tkinter.N)
 
-        nick_input = CTkEntry(master=window, width=213, placeholder_text='Ник бота')
-        nick_input.place(relx=0.13, rely=0.12, anchor=tkinter.N)
-        nick_input.insert(INSERT, config.get('Main', 'username'))
-        host_input = CTkEntry(master=window, width=250, placeholder_text='Адрес сервера формата ip:port / host')
-        host_input.place(relx=0.39, rely=0.12, anchor=tkinter.N)
-        host_input.insert(INSERT, config.get('Main', 'host'))
-        version_input = CTkEntry(master=window, width=70, placeholder_text='Версия')
-        version_input.place(relx=0.56, rely=0.12, anchor=tkinter.N)
-        version_input.insert(INSERT, config.get('Main', 'version'))
         cmd_input = CTkEntry(master=window, width=550, placeholder_text='Введите команду или текст для отправки от лица бота')
         cmd_input.place(relx=0.6, rely=0.90, anchor=tkinter.E)
 
         self.chat_log = CTkTextbox(master = window, width=750, height=420)
         self.chat_log.place(relx=0.4, rely=0.19, anchor=tkinter.N)
 
-        join_button = CTkButton(master=window, text='Запустить', command=bot_join)
-        join_button.place(relx=0.61, rely=0.14, anchor=tkinter.W)
         send_button = CTkButton(master=window, text='Отправить', command=send_message)
         send_button.place(relx=0.62, rely=0.90, anchor=tkinter.W)
 
@@ -250,6 +140,111 @@ class RakMineMain:
         def open_settings(): return RakMineSettings()
         settings_button = CTkButton(master=window, text='Настройки', command=open_settings, width=80, height=35, fg_color='#ff5d00', hover_color="#ad4c13")
         settings_button.place(relx=0.065, rely=0.01, anchor=tkinter.N)
+
+        if bot is not None: bot.quit()
+        
+        ip = host.split(':')
+        try: port = ip[1]
+        except IndexError: port = 25565
+
+        # Настройки
+        bot = mineflayer.createBot({
+            'host': ip[0],
+            'version': version,
+            'port': port,
+            'maps_outputDir': r"maps",
+            'maps_saveToFile': True,
+            'username': nick})
+
+        bot.loadPlugin(maps.inject)
+        bot.loadPlugin(pathfinder.pathfinder)
+
+        config.set('Main', 'username', nick)
+        config.set('Main', 'version', version)
+        config.set('Main', 'host', host)
+        with open('config.ini', "w") as config_file: config.write(config_file)
+            
+        if int(config.get('Settings', 'clear_chatlog')):
+            try: self.main_label.configure(text = check_stat())
+            except: pass
+
+            self.chat_log.delete(0.0, END)
+            window.title(f"{nick} | {ip[0]}:{port}")
+
+
+        @On(bot, 'error')
+        def error_handler(emittter, err): return self.log(f'| [BOT ERROR] {err}')
+
+
+        @On(bot, 'message')
+        def message_handler(emitter, message_info, *args):
+            if message_info.extra is None: return False
+
+            new_text = ''
+            for i in message_info.extra: new_text+=str(i.text)
+            return self.log(f'[MSG] {new_text}')
+
+
+        @On(bot, 'chat')
+        def message_handler(emitter, username, message, *args):
+            if message is None: return False
+            return self.log(f'[CHAT] {username}: {message}')
+
+
+        @On(bot, 'spawn')
+        def spawn_handle(*args):
+            self.in_game = True
+            try: return self.log(f"| [BOT INFO] Бот заспавнен на {bot.entity.position.toString()}!")
+            except: pass
+        
+
+        @On(bot, 'death')
+        def spawn_handle(*args):
+            self.in_game = True
+            try: return self.log(f"| [BOT INFO] Бот помер на {bot.entity.position.toString()}!")
+            except: pass
+        
+
+        @On(bot, "end")
+        def end_handle(emitter, reason):
+            global bot
+            self.in_game = False; bot = None
+            self.log(f"| [BOT INFO] Бот отключен от сервера!\n| Причина: {reason}")
+            return self.main_label.configure(text = check_stat())
+        
+        
+        @On(bot, 'move')
+        def move_handle(*args): return self.main_label.configure(text = check_stat())
+            
+        
+        @On(bot, "kicked")
+        def end_handle(emitter, reason, loddedin):
+            global bot
+            self.in_game = False; bot = None
+                
+            self.log(f"| [BOT INFO] Бот кикнут из сервера!\n| Причина: {reason}")
+            return self.main_label.configure(text = check_stat())
+            
+        
+        @On(bot, "health")
+        def end_handle(*args):
+            self.in_game = True
+            return self.main_label.configure(text = check_stat())
+            
+        
+        @On(bot, "forcedMove")
+        def forcedMove(*args):
+            self.in_game = True
+            try:
+                p = bot.entity.position
+                return self.log(f"| [BOT INFO] Бот телепортирован на {p.toString()}")
+            except: pass
+            
+        
+        @On(bot, 'windowOpen')
+        def window_handler(emitter, window): return print(window)
+        
+        window.mainloop()
     
     # Запись в chat log
     def log(self, message: str): return self.chat_log.insert(END, message + '\n')
@@ -258,7 +253,6 @@ class RakMineMain:
 if __name__ == "__main__":
     window = CTk()
     window.title("RakMine")
-    window.geometry('1000x620')
-    RakMineMain(window=window)
+    window.geometry('300x400')
+    RakMineJoin(window=window)
     #RakMineMain(window=window).log("текст")
-    window.mainloop()
