350cdab610e38d0b7da345aed3e61a3bf595168e
TastyBread123 <117034601+TastyBread123@users.noreply.github.com>
2023-05-03 21:31:37 +0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,25 +1,23 @@
-import configparser
-import tkinter
+import tkinter, sys
 
 from javascript import require, On #pip install javascript
-from customtkinter import *
-from tkinter.messagebox import *
+from tkinter.messagebox import showerror
 from functools import partial
+from threading import Thread
+from configparser import ConfigParser
+from customtkinter import *
+
 
 mineflayer = require('mineflayer')
 pathfinder = require('mineflayer-pathfinder')
 maps = require('mineflayer-maps')
 
-config = configparser.ConfigParser()
+config = ConfigParser()
 config.read('config.ini')
-prefix = '$'
-cmds = ['exit']
-
-bot = None
 
 
 # Получение статистики и возврат текта
-def check_stat():
+def check_stat(bot):
     if bot is None:
         return f'Ник: None\nЗдоровье: 0 | Сытость: 0 | Кол-во опыта: 0\nРежим игры: None | Coords: None'
     try:
@@ -29,26 +27,6 @@ def check_stat():
     except:
         return f'Ник: None\nЗдоровье: 0 | Сытость: 0 | Кол-во опыта: 0\nРежим игры: None | Coords: None'
 
-
-# Настройки
-class RakMineSettings:
-    def __init__(self) -> None:
-        def set_clear_log():
-            config.set('Settings', 'clear_chatlog', str(clear_log_var.get()))
-            with open('config.ini', "w") as config_file: config.write(config_file)
-            return True
-        
-        window = CTk()
-        window.title("RakMine | Настройки")
-        window.geometry('700x420')
-        
-        #очистка лога
-        clear_log_var = IntVar(window, value=int(config.get('Settings', 'clear_chatlog')))
-        clear_log = CTkCheckBox(master=window, variable=clear_log_var, text='Очистка чатлога после реконнекта', command=set_clear_log)
-        clear_log.place(relx=0.01, rely=0.01)
-
-        # Loop
-        window.mainloop()
 
 
 class RakMineJoin:
@@ -63,6 +41,7 @@ class RakMineJoin:
         version_input.place(relx=0.5, rely=0.4, anchor=tkinter.CENTER)
         version_input.insert(INSERT, config.get('Main', 'version'))
 
+
         def bot_join():            
             if nick_input.get() is None or nick_input.get() == '':
                 return showerror('Ошибка RakMine', 'Вы не ввели имя пользователя!')
@@ -71,78 +50,38 @@ class RakMineJoin:
             elif version_input.get() is None or version_input.get() == '':
                 return showerror('Ошибка RakMine', 'Вы не ввели версию игры!')
             
-            window = CTk()
+            window = CTkToplevel()
             window.title("RakMine")
             window.geometry('1000x620')
-            RakMineMain(window, host_input.get(), nick_input.get(), version_input.get())
+            Thread.__init__(RakMineMain(window, host_input.get(), nick_input.get(), version_input.get()))
+            
             
         join_button = CTkButton(master=window, text='Запустить', command=bot_join)
         join_button.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
         window.mainloop()
 
+# Настройки
+class RakMineSettings:
+    def __init__(self) -> None:
+        def set_clear_log():
+            config.set('Settings', 'clear_chatlog', str(clear_log_var.get()))
+            with open('config.ini', "w") as config_file: config.write(config_file)
+            return True
+        
+        window = CTkToplevel()
+        window.title("RakMine | Настройки")
+        window.geometry('700x420')
+        
+        #очистка лога
+        clear_log_var = IntVar(window, value=int(config.get('Settings', 'clear_chatlog')))
+        clear_log = CTkCheckBox(master=window, variable=clear_log_var, text='Очистка чатлога после реконнекта', command=set_clear_log)
+        clear_log.place(relx=0.01, rely=0.01)
+
 
 # Главное меню
 class RakMineMain:
     def __init__(self, window: CTk, host: str, nick: str, version: str) -> None:
-        def send_message():
-            cmd_input.delete(0.0, END)
-            return bot.chat(cmd_input.get())
-        
-        #bot.setControlState(control, state) | 'forward', 'back', 'left', 'right', # 'jump', 'sprint', 'sneak'
-        def control_click(control: str, button: CTkButton):
-            if bot is None: return showerror('Ошибка RakMine', 'Бот не запущен на сервере!')
-            if str(button._fg_color) == '#4CAF50':
-                bot.setControlState(control, True)
-                return button.configure(fg_color = '#eb1043')
-            else:
-                bot.setControlState(control, False)
-                return button.configure(fg_color = '#4CAF50')
-        
-        global bot
-
-        self.main_label = CTkLabel(master = window, text=str(check_stat()))
-        self.main_label.place(relx=0.5, rely = 0.03, anchor=tkinter.N)
-
-        cmd_input = CTkEntry(master=window, width=550, placeholder_text='Введите команду или текст для отправки от лица бота')
-        cmd_input.place(relx=0.6, rely=0.90, anchor=tkinter.E)
-
-        self.chat_log = CTkTextbox(master = window, width=750, height=420)
-        self.chat_log.place(relx=0.4, rely=0.19, anchor=tkinter.N)
-
-        send_button = CTkButton(master=window, text='Отправить', command=send_message)
-        send_button.place(relx=0.62, rely=0.90, anchor=tkinter.W)
-
-        #Клавиши управления
-        w_button = CTkButton(master=window, text='W', width=50, height=50, fg_color='#4CAF50', hover_color='#eb1043')
-        w_button.configure(command = partial(control_click, 'forward', w_button))
-        w_button.place(relx=0.85, rely=0.21, anchor=tkinter.W)
-        a_button = CTkButton(master=window, text='A', width=50, height=50, fg_color='#4CAF50', hover_color='#eb1043')
-        a_button.configure(command = partial(control_click, 'left', a_button))
-        a_button.place(relx=0.80, rely=0.3, anchor=tkinter.W)
-        s_button = CTkButton(master=window, text='S', width=50, height=50, fg_color='#4CAF50', hover_color='#eb1043')
-        s_button.configure(command = partial(control_click, 'back', s_button))
-        s_button.place(relx=0.85, rely=0.3, anchor=tkinter.W)
-        d_button = CTkButton(master=window, text='D', width=50, height=50, fg_color='#4CAF50', hover_color='#eb1043')
-        d_button.configure(command = partial(control_click, 'right', d_button))
-        d_button.place(relx=0.90, rely=0.3, anchor=tkinter.W)
-
-        sprint_button = CTkButton(master=window, text='Sprint', width=100, height=50, fg_color='#4CAF50', hover_color='#eb1043')
-        sprint_button.configure(command = partial(control_click, 'sprint', sprint_button))
-        sprint_button.place(relx=0.775, rely=0.45, anchor=tkinter.W)
-        jump_button = CTkButton(master=window, text='Jump', width=100, height=50, fg_color='#4CAF50', hover_color='#eb1043')
-        jump_button.configure(command = partial(control_click, 'jump', jump_button))
-        jump_button.place(relx=0.88, rely=0.45, anchor=tkinter.W)
-        sneak_button = CTkButton(master=window, text='Sneak', width=100, height=50, fg_color='#4CAF50', hover_color='#eb1043')
-        sneak_button.configure(command = partial(control_click, 'sneak', sneak_button))
-        sneak_button.place(relx=0.83, rely=0.55, anchor=tkinter.W)
-
-        def open_settings(): return RakMineSettings()
-        settings_button = CTkButton(master=window, text='Настройки', command=open_settings, width=80, height=35, fg_color='#ff5d00', hover_color="#ad4c13")
-        settings_button.place(relx=0.065, rely=0.01, anchor=tkinter.N)
-
-        if bot is not None: bot.quit()
-        
         ip = host.split(':')
         try: port = ip[1]
         except IndexError: port = 25565
@@ -158,6 +97,64 @@ class RakMineMain:
 
         bot.loadPlugin(maps.inject)
         bot.loadPlugin(pathfinder.pathfinder)
+        window.title(f"{nick} | {ip[0]}:{port}")
+
+        #bot.setControlState(control, state) | 'forward', 'back', 'left', 'right', # 'jump', 'sprint', 'sneak'
+        def control_click(control: str, button: CTkButton):
+            if bot is None: return showerror('Ошибка RakMine', 'Бот не запущен на сервере!')
+            if str(button._fg_color) == '#4CAF50':
+                try: bot.setControlState(control, True)
+                except: showerror('Ошибка RakMine', 'Бот не запущен на сервере!')
+                return button.configure(fg_color = '#EB1043', hover_color = '#FF0000')
+            else:
+                try: bot.setControlState(control, False)
+                except: showerror('Ошибка RakMine', 'Бот не запущен на сервере!')
+                return button.configure(fg_color = '#4CAF50', hover_color = '#00CC00')
+
+        def send_message(bot):
+            bot.chat(cmd_input.get())
+            return cmd_input.delete(0, END)
+
+        self.main_label = CTkLabel(master = window, text=str(check_stat(bot)))
+        self.main_label.place(relx=0.5, rely = 0.03, anchor=tkinter.N)
+
+
+        # Чат
+        self.chat_log = CTkTextbox(master = window, width=750, height=420)
+        self.chat_log.place(relx=0.4, rely=0.19, anchor=tkinter.N)
+        cmd_input = CTkEntry(master=window, width=550, placeholder_text='Введите команду или текст для отправки от лица бота')
+        cmd_input.place(relx=0.6, rely=0.90, anchor=tkinter.E)
+        send_button = CTkButton(master=window, text='Отправить', command=partial(send_message, bot))
+        send_button.place(relx=0.62, rely=0.90, anchor=tkinter.W)
+
+        #Клавиши управления
+        w_button = CTkButton(master=window, text='W', width=50, height=50, fg_color='#4CAF50', hover_color='#00CC00')
+        w_button.configure(command = partial(control_click, 'forward', w_button))
+        w_button.place(relx=0.85, rely=0.21, anchor=tkinter.W)
+        a_button = CTkButton(master=window, text='A', width=50, height=50, fg_color='#4CAF50', hover_color='#00CC00')
+        a_button.configure(command = partial(control_click, 'left', a_button))
+        a_button.place(relx=0.80, rely=0.3, anchor=tkinter.W)
+        s_button = CTkButton(master=window, text='S', width=50, height=50, fg_color='#4CAF50', hover_color='#00CC00')
+        s_button.configure(command = partial(control_click, 'back', s_button))
+        s_button.place(relx=0.85, rely=0.3, anchor=tkinter.W)
+        d_button = CTkButton(master=window, text='D', width=50, height=50, fg_color='#4CAF50', hover_color='#00CC00')
+        d_button.configure(command = partial(control_click, 'right', d_button))
+        d_button.place(relx=0.90, rely=0.3, anchor=tkinter.W)
+
+        sprint_button = CTkButton(master=window, text='Sprint', width=100, height=50, fg_color='#4CAF50', hover_color='#00CC00')
+        sprint_button.configure(command = partial(control_click, 'sprint', sprint_button))
+        sprint_button.place(relx=0.775, rely=0.45, anchor=tkinter.W)
+        jump_button = CTkButton(master=window, text='Jump', width=100, height=50, fg_color='#4CAF50', hover_color='#00CC00')
+        jump_button.configure(command = partial(control_click, 'jump', jump_button))
+        jump_button.place(relx=0.88, rely=0.45, anchor=tkinter.W)
+        sneak_button = CTkButton(master=window, text='Sneak', width=100, height=50, fg_color='#4CAF50', hover_color='#00CC00')
+        sneak_button.configure(command = partial(control_click, 'sneak', sneak_button))
+        sneak_button.place(relx=0.83, rely=0.55, anchor=tkinter.W)
+
+        #Меню настроек
+        def open_settings(): return RakMineSettings()
+        settings_button = CTkButton(master=window, text='Настройки', command=open_settings, width=80, height=35, fg_color='#ff5d00', hover_color="#ad4c13")
+        settings_button.place(relx=0.065, rely=0.01, anchor=tkinter.N)
 
         config.set('Main', 'username', nick)
         config.set('Main', 'version', version)
@@ -165,16 +162,14 @@ class RakMineMain:
         with open('config.ini', "w") as config_file: config.write(config_file)
             
         if int(config.get('Settings', 'clear_chatlog')):
-            try: self.main_label.configure(text = check_stat())
-            except: pass
-
-            self.chat_log.delete(0.0, END)
-            window.title(f"{nick} | {ip[0]}:{port}")
+            try: 
+                self.main_label.configure(text = check_stat(bot))
+                self.chat_log.delete(0.0, END)
+            except: sys.exit(1)
 
 
         @On(bot, 'error')
         def error_handler(emittter, err): return self.log(f'| [BOT ERROR] {err}')
-
 
         @On(bot, 'message')
         def message_handler(emitter, message_info, *args):
@@ -184,70 +179,63 @@ class RakMineMain:
             for i in message_info.extra: new_text+=str(i.text)
             return self.log(f'[MSG] {new_text}')
 
-
         @On(bot, 'chat')
         def message_handler(emitter, username, message, *args):
             if message is None: return False
             return self.log(f'[CHAT] {username}: {message}')
 
-
         @On(bot, 'spawn')
         def spawn_handle(*args):
-            self.in_game = True
             try: return self.log(f"| [BOT INFO] Бот заспавнен на {bot.entity.position.toString()}!")
             except: pass
         
-
         @On(bot, 'death')
         def spawn_handle(*args):
-            self.in_game = True
             try: return self.log(f"| [BOT INFO] Бот помер на {bot.entity.position.toString()}!")
             except: pass
         
-
-        @On(bot, "end")
-        def end_handle(emitter, reason):
-            global bot
-            self.in_game = False; bot = None
-            self.log(f"| [BOT INFO] Бот отключен от сервера!\n| Причина: {reason}")
-            return self.main_label.configure(text = check_stat())
-        
-        
         @On(bot, 'move')
-        def move_handle(*args): return self.main_label.configure(text = check_stat())
-            
-        
-        @On(bot, "kicked")
-        def end_handle(emitter, reason, loddedin):
-            global bot
-            self.in_game = False; bot = None
-                
-            self.log(f"| [BOT INFO] Бот кикнут из сервера!\n| Причина: {reason}")
-            return self.main_label.configure(text = check_stat())
-            
+        def move_handle(*args): 
+            try: return self.main_label.configure(text = check_stat(bot))
+            except: return sys.exit(1)
         
         @On(bot, "health")
-        def end_handle(*args):
-            self.in_game = True
-            return self.main_label.configure(text = check_stat())
-            
+        def end_handle(*args): 
+            try: return self.main_label.configure(text = check_stat(bot))
+            except: return sys.exit(1)
         
         @On(bot, "forcedMove")
         def forcedMove(*args):
-            self.in_game = True
-            try:
-                p = bot.entity.position
-                return self.log(f"| [BOT INFO] Бот телепортирован на {p.toString()}")
-            except: pass
-            
+            try: 
+                self.log(f"| [BOT INFO] Бот телепортирован на {bot.entity.position.toString()}")
+                return self.main_label.configure(text = check_stat(bot))
+            except: sys.exit(1)
         
         @On(bot, 'windowOpen')
         def window_handler(emitter, window): return print(window)
+
+        @On(bot, "kicked")
+        def end_handle(emitter, reason, loddedin):
+            global bot
+            bot = None
+            try:
+                self.log(f"| [BOT INFO] Бот кикнут из сервера!\n| Причина: {reason}")
+                return self.main_label.configure(text = check_stat(bot))
+            except: sys.exit(1)
         
-        window.mainloop()
+        @On(bot, "end")
+        def end_handle(emitter, reason):
+            global bot
+            bot = None
+            try:
+                self.log(f"| [BOT INFO] Бот отключен от сервера!\n| Причина: {reason}")
+                return self.main_label.configure(text = check_stat(bot))
+            except: sys.exit(1)
     
     # Запись в chat log
-    def log(self, message: str): return self.chat_log.insert(END, message + '\n')
+    def log(self, message: str):
+        try: return self.chat_log.insert(END, message + '\n')
+        except: return sys.exit(1)
 
 # Начало
 if __name__ == "__main__":
